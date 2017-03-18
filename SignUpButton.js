a3a705591ec5360a60422f8b59d255b4c474b725
Sandeep Patidar <sandeep.patidar.fidel@gmail.com>
2017-03-18 07:42:26 +0530

    Initial project set-up

---
+++ SignUpButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Button,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    alignSelf: 'center',
+    borderRadius: 4,
+    borderWidth: 3,
+    marginTop: 5,
+    width: 60,
+  },
+});
+
+const SignUpButton = props => (
+  <View style={[styles.containerStyle, props.containerStyles]}>
+    <Button {...props} />
+  </View>
+);
+
+SignUpButton.propTypes = {
+  containerStyles: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.number]),
+};
+
+SignUpButton.defaultProps = {
+  containerStyles: {},
+};
+
+export default SignUpButton;
